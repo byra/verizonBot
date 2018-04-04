9d912e3c27d2f311a3cb44fecf8646990cd62581
byra <manikantamaheshbyra@icloud.com>
2018-04-04 17:01:50 -0500

    added speech to text for chrome

--- src/containers/ChatArea.js
+++ src/containers/ChatArea.js
@@ -4,16 +4,65 @@ import {connect} from "react-redux";
 import {onRequest} from "../actions/chatMessages";
 import Chat from "./Chat"
 import voice from "../assets/images/voice.svg"
+import listening from "../assets/images/listening.svg"
 
 
 class ChatArea extends Component {
 
     constructor(props) {
         super(props);
+        this.speechToText = this.speechToText.bind(this);
+        this.sendReqOnEnter = this.sendReqOnEnter.bind(this);
+        this.sendReqOnButton = this.sendReqOnButton.bind(this);
+        this.userMsg = React.createRef();
     };
 
+    sendReqOnEnter(event) {
+        if (event.key === 'Enter' && this.userMsg.value.trim() !== "") {
+            this.props.fetchResponse(this.userMsg.value.trim());
+            this.userMsg.value = ""
+        }
+    }
+
+    sendReqOnButton() {
+        if (this.userMsg.value.trim() !== "") {
+            this.props.fetchResponse(this.userMsg.value.trim());
+            this.userMsg.value = ""
+        }
+    }
+
+    speechToText() {
+        if (!!window.chrome && !!window.chrome.webstore) {
+
+            let recognition = new webkitSpeechRecognition();
+
+            recognition.continuous = true;
+            recognition.onresult = function (event) {
+                for (let i = event.resultIndex; i < event.results.length; ++i) {
+                    if (event.results[i].isFinal) {
+                        document.getElementById("inputText").value += event.results[i][0].transcript;
+                    }
+                }
+            };
+
+            if(document.getElementById("mic").alt === "ready"){
+                document.getElementById("mic").src = listening;
+                document.getElementById("mic").alt = "listening";
+                recognition.start();
+            }
+            else{
+                document.getElementById("mic").src = voice;
+                document.getElementById("mic").alt = "ready";
+                recognition.stop();
+            }
+
+        }
+    }
+
+
     render() {
-        let userMsg="";
+
+
         return (
             <div className="chat-container">
                 <div className="row justify-content-start">
@@ -30,32 +79,17 @@ class ChatArea extends Component {
                                 <div className="row justify-content-start input-group input-group-lg">
                                     <div className="col">
                                         <div className="d-flex justify-content-between">
-                                            <input type="text" className="form-control mr-1" aria-label="Large" aria-describedby="inputGroup-sizing-sm"
-                                                   ref={node =>
-                                                   {
-                                                       userMsg = node
-                                                   }
-                                                   }
-                                                   onKeyPress={
-                                                       (event) => {
-                                                           if (event.key === 'Enter' && userMsg.value.trim() !== "") {
-                                                               this.props.fetchResponse(userMsg.value.trim());
-                                                               userMsg.value = "";
-                                                           }
-                                                       }
-                                                   }
+                                            <input type="text" id="inputText" className="form-control mr-2"
+                                                   aria-label="Large" aria-describedby="inputGroup-sizing-sm"
+                                                   ref={node => {
+                                                       this.userMsg = node;
+                                                   }}
+                                                   onKeyPress={this.sendReqOnEnter}
                                             />
-                                            <button className="btn btn-primary" onClick={
-                                                (event) => {
-                                                    if (userMsg.value.trim() !== "") {
-                                                        this.props.fetchResponse(userMsg.value.trim());
-                                                        userMsg.value = "";
-                                                    }
-                                                }
-                                            }>
+                                            <button className="btn btn-primary mr-2" onClick={this.sendReqOnButton}>
                                                 Send
                                             </button>
-                                            <img src={voice}/>
+                                            <img src={voice} onClick={this.speechToText} id="mic" alt="ready"/>
                                         </div>
                                     </div>
                                 </div>
@@ -68,14 +102,12 @@ class ChatArea extends Component {
     };
 }
 
-const mapStateToProps = (state) =>{
-    return{
-
-    };
+const mapStateToProps = (state) => {
+    return {};
 };
 
-const mapDispatchToProps = (dispatch) =>{
-    return{
+const mapDispatchToProps = (dispatch) => {
+    return {
         fetchResponse: (request) => dispatch(onRequest(request)),
     }
 };
